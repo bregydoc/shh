1e6da799f4dbbe9e668af1e843f1fca67a020cd7
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-12-19 02:22:03 -0500

    little fix

--- service.go
+++ service.go
@@ -15,6 +15,7 @@ func (w *Wizard) runRPCService() error {
 	if !strings.HasPrefix(w.rpcPort, ":") {
 		port = ":" + port
 	}
+	
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
